5f1e4d8bb9b5a31f14d47520a16a48c34ba192e6
ntdkhang <ntdkhang143@gmail.com>
2024-02-21 19:48:03 -0600

    project creation

---
+++ CarAssist/CarAssist/CarAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAssistApp.swift
+//  CarAssist
+//
+//  Created by Khang Nguyen on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CarAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarAssist/CarAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarAssist
+//
+//  Created by Khang Nguyen on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
