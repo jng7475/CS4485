1a40c5a42cd03f3eeed1f15a847c17a38e8fa467
ntdkhang <ntdkhang143@gmail.com>
2024-02-23 18:29:37 -0600

    signup view

---
+++ CarAssist/CarAssist/AuthenticationView.swift
@@ -0,0 +1,32 @@
+//
+//  AuthenticationView.swift
+//  CarAssist
+//
+//  Created by Khang Nguyen on 2/22/24.
+//
+
+import SwiftUI
+
+import SwiftUI
+import Combine
+
+struct AuthenticationView: View {
+  @EnvironmentObject var viewModel: AuthenticationViewModel
+
+  var body: some View {
+    VStack {
+      switch viewModel.flow {
+      case .login:
+        LoginView()
+          .environmentObject(viewModel)
+      case .signUp:
+        SignupView()
+          .environmentObject(viewModel)
+      }
+    }
+  }
+}
+
+#Preview {
+    AuthenticationView()
+}
